d50c5d90a93823d5f7550b49de78e0d8ed28e28d
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-07-19 21:47:50 -0400

    Create an Apollo Provider to make every request work with the Apollo server

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,54 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
+import {ApolloClient,ApolloProvider} from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import SavedBooks from './pages/SavedBooks';
+import SearchBooks from './pages/SearchBooks';
 import Navbar from './components/Navbar';
+
+
+
+const client = new ApolloClient ({
+  request: (operation) => {
+    // get the authentication token from local storage if it exists
+   const token = localStorage.getItem("id_token");
+
+
+   operation.setContext ({
+    headers: {
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  });
+},
+  uri: "/graphql"
+});
+
+
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+          <Navbar />
+          <Switch>
+          <Route 
+                path="/" 
+                element= {SearchBooks}/>
+      
+              <Route 
+                path="/savedBooks" 
+                element={SavedBooks} />
+
+              <Route 
+                path="/signup" 
+                element={Signup}/>
+           
+              <Route 
+                path="/me" 
+                element={Login}/>
+            
+          </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
